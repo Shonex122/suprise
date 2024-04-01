44eeff92bf0c93f13c1258d8ddffc7b2a60dbf70
Mosh Hamedani <omoshola808@gmail.com>
2024-04-01 08:43:55 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let container = document.getElementById("container");
+
+container.innerHTML = "<button onclick='HNM()'> Eniola, click Me!</button>";
+
+function HNM() {
+  container.innerHTML +=
+    "<h1> Dear Eniola&#9829;,  All i want in this new month is our love to keep growing stronger daily, i love you HAPPY NEW MONTH.</h1> ";
+}
